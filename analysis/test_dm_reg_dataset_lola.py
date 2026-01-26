04a336165d80e05617896094105ff010066f9d82
Lola O <146087172+Lola-Audrey@users.noreply.github.com>
2026-01-26 08:55:26 +0000

    Add test for most recent unresolved diagnosis

--- analysis/test_dm_reg_dataset_lola.py
+++ analysis/test_dm_reg_dataset_lola.py
@@ -36,6 +36,7 @@ test_data = {
         "patients": {"date_of_birth": date(2000, 1, 1)},
         "practice_registrations": [{"start_date": date(2014, 7, 10)}],
         "clinical_events": [
+            {"date": date(2014, 7, 9), "snomedct_code": "111552007"},
             {"date": date(2015, 5, 8), "snomedct_code": "315051004"},
             {"date": date(2015, 5, 9), "snomedct_code": "111552007"},
         ],
@@ -44,7 +45,7 @@ test_data = {
             "pat_age": 24,
             "reg_date": date(2014, 7, 10),
             "dereg_date": None,
-            "dm_dat": date(2015, 5, 9),
+            "dm_dat": date(2014, 7, 9),
             "dmlat_dat": date(2015, 5, 9),
             "dmres_dat": date(2015, 5, 8),
         },
